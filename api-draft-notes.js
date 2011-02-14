54df9d04266f2eccf994b8509f47b946645e78dc
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-02-13 19:45:27 -0500

    refactored starter fn, it still does not work, started api draft notes

---
+++ api-draft-notes.js
@@ -0,0 +1,6 @@
+csvlib( csvstring ).rows() // Returns an array of row strings
+
+csvlib( csvstring ).data() // Returns an array of data row strings
+
+csvlib( csvstring ).header() // Returns an array of header properties
+
--- csv.js
+++ csv.js
@@ -1,13 +1,5 @@
-var createRefinementUI = function( csv ) {
+var csv = function( csv ) {
   var _rows = csv.split('\n'),
       _header = _rows[0].split(','),
-      _data = _rows.slice( 1 ),
-      propsSelect = $('[name=set]').clone().get(0),
-      ocdPropertiesUL = '<ul class="ocdproperties"><li></li><li>start</li><li>end</li><li>geometry</li></ul>',
-      userPropertiesUL = $('<ul>', {
-        className: 'userProperties',
-      });
-
-  _.each( _header , function( iterator, property ){
-    userPropertiesUL.append('<li>' + iterator + '</li>')
-  });
+      _data = _rows.slice( 1 );
+}
