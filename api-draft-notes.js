0d6730cc20f2b9ee29a69cf813b38f9c803fb597
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-02-13 20:24:37 -0500

    updated api draft

--- api-draft-notes.js
+++ api-draft-notes.js
@@ -2,5 +2,9 @@ csvlib( csvstring ).rows() // Returns an array of row strings
 
 csvlib( csvstring ).data() // Returns an array of data row strings
 
-csvlib( csvstring ).header() // Returns an array of header properties
+csvlib( csvstring ).header() // Returns the header string
+
+csvlib( csvstring ).props() // Returns an array of properties
+
+csvlib( csvstring ).header().props() // Returns an array of header properties
 
