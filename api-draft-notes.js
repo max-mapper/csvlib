389de4f1f804e0a0ca33af566d8c9c80b6efbd8a
maxogden <max@maxogden.com>
2011-02-13 21:18:07 -0500

    rudimentary implementation and adding test page

--- api-draft-notes.js
+++ api-draft-notes.js
@@ -1,10 +1,7 @@
-csvlib( csvstring ).rows() // Returns an array of row strings
+csv.rows(csvstring) // Returns an array of row strings
 
-csvlib( csvstring ).data() // Returns an array of data row strings
+csv.data(csv.rows(csvstring)) // Returns an array of data row strings
 
-csvlib( csvstring ).header() // Returns the header string
+csv.header(csv.rows(csvstring)) // Returns the header string
 
-csvlib( csvstring ).props() // Returns an array of properties
-
-csvlib( csvstring ).header().props() // Returns an array of header properties
-
+csv.properties(csv.header(csv.rows(csvstring))) // Returns an array of header properties
--- csv.js
+++ csv.js
@@ -1,5 +1,61 @@
-var csv = function( csv ) {
-  var _rows = csv.split('\n'),
-      _header = _rows[0].split(','),
-      _data = _rows.slice( 1 );
-}
+(function() {
+  var csv = this.csv = {};
+  
+  // Export the csv object for CommonJS
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = csv;
+  }
+      
+  csv.rows = function(csv) {
+    return csv.split('\n');
+  }
+  
+  csv.header = function(rows) {
+    return rows[0];
+  }
+  
+  csv.data = function(rows) {
+    return rows.slice(1);
+  }
+  
+  csv.properties = function(row) {
+    return row.split(',');
+  }
+  
+  // from http://stackoverflow.com/questions/2210437/flattening-a-complex-json-object-for-mvc-binding
+  csv.flattenJSON = function(json){
+    var nj = {},
+      walk = function(j){
+        var jp;
+        for(var prop in j){
+          jp = j[prop];
+          if(jp.toString() === "[object Object]"){
+            walk(jp);
+          }else{
+            nj[prop] = jp;
+          }
+        }
+      };
+    walk(json);
+    return nj;
+  }
+  
+  csv.buildHeaders = function(json) {
+    var keys = [];
+    var flatRow = csv.flattenJSON(json);
+    for (var key in flatRow) {
+      keys.push(key);
+    }
+    return keys.join(',') + sep;
+  }
+  
+  csv.buildRow = function(json) {
+    var keys = [];
+    var flatRow = csv.flattenJSON(json);
+    for (var key in flatRow) {
+      keys.push(flatRow[key]);
+    }
+    return keys.join(',') + sep;
+  }
+
+})();
